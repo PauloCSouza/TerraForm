722453ac9c1f8330dd82039867d7546e01a8e489
Paulo Souza <developer.adsm@gmail.com>
2023-02-16 13:42:15 -0300

    ajuste aws bucket s3

--- bucket-s3/main.tf
+++ bucket-s3/main.tf
@@ -1,9 +1,8 @@
 terraform {
   required_version = "1.3.5"
-
   required_providers {
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
       version = "4.41.0"
     }
   }
@@ -15,9 +14,8 @@ provider "aws" {
 
 resource "aws_s3_bucket" "my-first-bucket-s3-tf" {
   bucket = "my-first-bucket"
-  acl    = "private"
 
-  tags   = {
+  tags = {
     Name        = "My Bucket"
     Environment = "Dev"
     Managedby   = "Terraform"
